ad16ca3043e8510896e517eee9c2b1810db67fc4
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-10-13 00:38:04 +0900

    tutorial part2 routesを使って選択したハートの内容だけを表示するページへリンクできる

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,41 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _pushSaved() {
+    /*Navigatorにpushするとページ遷移することができる。popだと元のページにもどる*/
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      /*_savedでアクティブなハートだけを絞ってる*/
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        },
+      );
+      final divided = ListTile.divideTiles(
+        context: context,
+        tiles: tiles,
+      ).toList();
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
+      );
+    }));
   }
 
   Widget _buildSuggestions() {
