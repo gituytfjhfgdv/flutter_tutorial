665665197d8a8a4339012d14a8ef6c51288f4330
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-10-16 23:45:13 +0900

    コメント追加

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   runApp(MyApp());
 }
 
+// 静的なコンテンツなのでStatelessWidget
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -65,11 +66,11 @@ class MyApp extends StatelessWidget {
       padding: const EdgeInsets.all(32),
       child: Text(
         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
         softWrap: true,
       ),
     );
