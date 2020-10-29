2f845e8eda608c1ba020475ffb6d1a8f8d3fcda1
gituytfjhfgdv <de_syu3939@yahoo.co.jp>
2020-10-29 23:51:30 +0900

    使ってない記述の削除とアイコンの大きさを変更しようと試みる

--- lib/main.dart
+++ lib/main.dart
@@ -12,79 +12,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
-
     Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
 
     Widget awsomeIcon = Container(
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        IconButton(
-            // Use the FaIcon Widget + FontAwesomeIcons class for the IconData
-            icon: FaIcon(FontAwesomeIcons.smile),
-            onPressed: () {
-              print("Pressed");
-            },
-            color: color),
+        SizedBox(
+          height: 50.0,
+          width: 50.0,
+          child: IconButton(
+              // Use the FaIcon Widget + FontAwesomeIcons class for the IconData
+              icon: FaIcon(FontAwesomeIcons.smile),
+              onPressed: () {
+                print("Pressed");
+              },
+              color: color),
+        ),
         IconButton(
             // Use the FaIcon Widget + FontAwesomeIcons class for the IconData
             icon: FaIcon(FontAwesomeIcons.angry),
